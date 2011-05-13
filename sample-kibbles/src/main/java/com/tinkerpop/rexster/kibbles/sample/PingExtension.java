852dc73111e1837b8daa8a0a0adcffab77b84f21
stephen mallette <spmva@genoprime.com>
2011-05-13 11:58:02 -0400

    added some specific error handling to the PingExtension.

--- sample-kibbles/src/main/java/com/tinkerpop/rexster/kibbles/sample/PingExtension.java
+++ sample-kibbles/src/main/java/com/tinkerpop/rexster/kibbles/sample/PingExtension.java
@@ -2,15 +2,9 @@ package com.tinkerpop.rexster.kibbles.sample;
 
 import com.tinkerpop.blueprints.pgm.Graph;
 import com.tinkerpop.rexster.RexsterResourceContext;
-import com.tinkerpop.rexster.extension.AbstractRexsterExtension;
-import com.tinkerpop.rexster.extension.ExtensionDefinition;
-import com.tinkerpop.rexster.extension.ExtensionDescriptor;
-import com.tinkerpop.rexster.extension.ExtensionNaming;
-import com.tinkerpop.rexster.extension.ExtensionPoint;
-import com.tinkerpop.rexster.extension.ExtensionRequestParameter;
-import com.tinkerpop.rexster.extension.ExtensionResponse;
-import com.tinkerpop.rexster.extension.RexsterContext;
+import com.tinkerpop.rexster.extension.*;
 
+import javax.ws.rs.core.Response;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,6 +30,16 @@ public class PingExtension extends AbstractSampleExtension {
     public ExtensionResponse evaluatePing(@RexsterContext RexsterResourceContext context,
                                           @RexsterContext Graph graph,
                                           @ExtensionRequestParameter(name="reply", description="a value to reply with") String reply) {
+        if (reply == null || reply.isEmpty()) {
+            ExtensionMethod extMethod = context.getExtensionMethod();
+            return ExtensionResponse.error(
+                    "the reply parameter cannot be empty",
+                    null,
+                    Response.Status.BAD_REQUEST.getStatusCode(),
+                    null,
+                    generateErrorJson(extMethod.getExtensionApiAsJson()));
+        }
+
         Map<String, String> map = new HashMap<String, String>();
         map.put("ping", reply);
         return ExtensionResponse.ok(map);
