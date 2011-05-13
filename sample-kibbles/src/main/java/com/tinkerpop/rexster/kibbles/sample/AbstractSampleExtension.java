48831d69d9c6aa03a434603e16606f7d68be6ee6
stephen mallette <spmva@genoprime.com>
2011-05-13 07:45:26 -0400

    changed default namespace for samples project to tp-sample

--- sample-kibbles/src/main/java/com/tinkerpop/rexster/kibbles/sample/AbstractSampleExtension.java
+++ sample-kibbles/src/main/java/com/tinkerpop/rexster/kibbles/sample/AbstractSampleExtension.java
@@ -6,5 +6,5 @@ import com.tinkerpop.rexster.extension.AbstractRexsterExtension;
  * Base class for samples.
  */
 public abstract class AbstractSampleExtension extends AbstractRexsterExtension {
-    public static final String EXTENSION_NAMESPACE = "tp";
+    public static final String EXTENSION_NAMESPACE = "tp-sample";
 }
